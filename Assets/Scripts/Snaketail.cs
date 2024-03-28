b4592d966fef681e6d59b453ffddcbecb307ed4b
M P Madhan <assassinisnotme@gmail.com>
2024-03-28 22:01:13 +0530

    Oops missed that reference

--- Assets/Scripts/Snaketail.cs
+++ Assets/Scripts/Snaketail.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 
 public class Snaketail : MonoBehaviour
 {
-    private Dictionary<Vector2Int, string> _directionTriggerMap = new()
-    {
-        { Vector2Int.up, StringConsts.UP },
-        { Vector2Int.left, StringConsts.LEFT },
-        { Vector2Int.down, StringConsts.DOWN },
-        { Vector2Int.right, StringConsts.RIGHT }
-    };
 
     [SerializeField] Animator _animator;
 
     public void Move(Vector3 position, Vector2Int direction)
     {
-        if(_directionTriggerMap.TryGetValue(direction, out string trigger))
+        if(StringConsts._directionTriggerMap.TryGetValue(direction, out string trigger))
         {
             _animator.SetTrigger(trigger);
         }
